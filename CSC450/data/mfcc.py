7ba23374d761007d3b2a0f7f64653899d4260a5d
Jo Slayton <ajs41@missouristate.edu>
2020-02-12 14:29:22 -0600

    implemented getFiles method in mfcc.py

--- CSC450/data/mfcc.py
+++ CSC450/data/mfcc.py
@@ -6,9 +6,17 @@ WILL
     and probably other things as I think of them
 '''
 import aubio
+from os import system, remove
+
+# gets a list of filenames
+# will include filenames.txt
+def getFiles(root):
+    system("dir /b /a-d " + root + "filenames.txt")
+    return(open(root + "filenames.txt").read().split())
 
 def main():
-    root = "c:\\Users\\alyss\\Documents\\EDGAR\CSC450\\data\\smallSet"
+    root = "c:\\Users\\alyss\\Documents\\EDGAR\CSC450\\data\\smallSet"  # root for file directory
+
     print("running...")
 
 if __name__ == "__main__":
