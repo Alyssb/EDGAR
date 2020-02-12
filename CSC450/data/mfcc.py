a3a72d9e2fa677e3bb16f62e82cb443d9f2a0f67
Jo Slayton <ajs41@missouristate.edu>
2020-02-12 13:48:50 -0600

    initial commit of mfcc.py

---
+++ CSC450/data/mfcc.py
@@ -0,0 +1,15 @@
+'''
+This is a script which 
+WILL
+    calculate mel frequency cepstral coefficients for a given vile using aubio
+    print them out
+    and probably other things as I think of them
+'''
+import aubio
+
+def main():
+    root = "c:\\Users\\alyss\\Documents\\EDGAR\CSC450\\data\\smallSet"
+    print("running...")
+
+if __name__ == "__main__":
+    main()
