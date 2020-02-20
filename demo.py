c62b4a9abda06a849767d68bf1287577a919dd1b
cjchamp4now <alan.coryjackson@gmail.com>
2020-02-19 20:02:19 -0600

    Delete audio file just created via demo.py, added check to verify file existance, can be included afterwards for testing to verify deletion as well. -updated demo.py, added import os statements

--- demo.py
+++ demo.py
@@ -6,7 +6,17 @@ from spectrogram import get_spectrogram
 sys.path.append('./recording_audio/')
 from record_audio import record_audio
 
+from os import remove
+from os.path import exists
+
 input_name = record_audio()
 print("Audio file: " + input_name + " created")
 get_Metrics(input_name ,0, 512, 128, "delta")
 get_spectrogram(input_name, 0)
+
+if(exists(input_name)):
+    remove(input_name)
+    print("file deleted")
+else:
+    print("file error")
+
