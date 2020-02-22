2b2e3f8453466fa11a64bae44dd3cfd9bbb49b79
Melanie Jelinek <jelinek076@live.missouristate.edu>
2020-02-22 15:38:16 -0600

    log mel spectrogram implemented in EDGAR_demo.py

--- EDGAR_demo.py
+++ EDGAR_demo.py
@@ -1,18 +1,21 @@
 import sys
 sys.path.append('./CSC450/')
-from audioMetrics import get_Metrics
-from spectrogram import get_spectrogram
+from get_mfcc import get_MFCC
+from get_spectrogram import get_spectrogram
+from get_melspectrogram import get_MelSpectrogram
 
 sys.path.append('./recording_audio/')
-from record_audio import record_audio
+from get_audio import get_audio
 
 from os import remove
 from os.path import exists
 
-input_name = record_audio()
+input_name = get_audio()
 print("Audio file: " + input_name + " created")
-get_Metrics(input_name ,0, 512, 128, "delta")
+get_MFCC(input_name, 0, 512, 128, "delta")
 get_spectrogram(input_name, 0)
+get_MelSpectrogram(input_name)
+
 
 if(exists(input_name)):
     remove(input_name)
