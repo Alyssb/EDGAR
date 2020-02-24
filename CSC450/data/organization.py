2b6468434c62cfd0af134dc084a505be4f35e83d
Jo Slayton <ajs41@missouristate.edu>
2020-02-23 21:41:13 -0600

    initial commit of organization.py

---
+++ CSC450/data/organization.py
@@ -0,0 +1,17 @@
+'''
+Alyssa Slayton 2020
+
+This is going to be a bit of a bigger commitment than I originally anticipated...
+
+Initial format of data:
+
+'''
+'''
+Final format of data tree:
+'''
+
+def main():
+    print("hello world.")
+
+if __name__ == '__main__':
+    main()
