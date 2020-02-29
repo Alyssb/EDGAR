249643c2a1af56333906e3f1be0fe6d4aa46e936
Jo Slayton <ajs41@missouristate.edu>
2020-02-28 18:00:55 -0600

    almost done documenting. Changed file naming scheme to be by time.

--- get_audio.py
+++ get_audio.py
@@ -1,34 +1,50 @@
+'''
+Stephen Carr
+CSC450 SP2020 GROUP 4
+Missouri State University
+'''
+'''
+we should probably modularize this a bit
+'''
 import pyaudio
 import wave
 from pydub import AudioSegment
 from pydub.playback import play
 import random
+import time
 
 
 def get_audio():
+    '''
+    will begin recording when R is pressed
 
-    custom_seconds = input("how long do you want to record? ")
+    custom_seconds: number of seconds to record
+    num_recordings: number of recordings to be taken
+    '''
+
+    custom_seconds = input("how many seconds do you want to record? ")
     num_recordings = input("How many recordings of that length would you like? ")
-    #start = raw_input("Press 'r' to beging recording. ") #is this python 2?
     start = input("type and enter 'r' to begin recording. ")
 
-
+    # if r or R typed
     if start == 'r' or 'R':
+
         for b in range(int(num_recordings)):
-            chunk = 1024  # Record in chunks of 1024 samples
-            sample_format = pyaudio.paInt16  # 16 bits per sample
-            channels = 1
-            fs = 44100  # Record at 44100 samples per second
+            chunk = 1024                        # Record in chunks of 1024 samples
+            sample_format = pyaudio.paInt16     # 16 bits per sample
+            channels = 1                        # only using one channel
+            fs = 44100                          # Record at 44100 samples per second
             seconds = int(custom_seconds)
 
-            unique_num = random.randint(1,10000)
-
+            # get the current time for use in unique filename
+            unique_num = int(time.time())
             filename = 'live_audio/Output' + str(unique_num) + '.wav'
 
             p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
             print('Recording')
 
+            # open a recording stream
             stream = p.open(format=sample_format,
                             channels=channels,
                             rate=fs,
@@ -38,12 +54,12 @@ def get_audio():
             frames = []  # Initialize array to store frames
 
 
-            # Store data in chunks for 3 seconds i.e. recording
+            # Store data in chunks
             for i in range(0, int(fs / chunk * seconds)):
                 data = stream.read(chunk)
                 frames.append(data)
 
-        # Stop and close the stream
+            # Stop and close the stream
             stream.stop_stream()
             stream.close()
             # Terminate the PortAudio interface
@@ -64,3 +80,7 @@ def get_audio():
 
     else:
         print("Incorrect key. Run program again.")
+
+
+if __name__ == '__main__':
+    get_audio()
