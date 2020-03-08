a04f2411fdd577d283faa0280635f01f45ef91ac
Jo Slayton <ajs41@missouristate.edu>
2020-03-08 01:26:19 -0600

    created function displaySpectrogram in get_melspectrogram.py

--- get_melspectrogram.py
+++ get_melspectrogram.py
@@ -15,7 +15,6 @@ from numpy import max, dstack, save
 import matplotlib.pyplot as plt
 import time
 
-
 ################################### class melSpectrogram ######################################
 class melSpectrogram:
 
@@ -24,32 +23,34 @@ class melSpectrogram:
         self.samplerate = 16000
 
     def get_MelSpectrogram(self):
-        y, sr = librosa.load(self.source_filename, self.samplerate)
-        S = librosa.feature.melspectrogram(y, sr, n_mels=40, n_fft=512, fmin=300, fmax=8000)
+        # I don't really know what these do, someone else should document this
+        y, self.sr = librosa.load(self.source_filename, self.samplerate)
+        S = librosa.feature.melspectrogram(y, self.sr, n_mels=40, n_fft=512, fmin=300, fmax=8000)
         ms_delta = librosa.feature.delta(S)
         ms_delta2 = librosa.feature.delta(S, order=2)
 
         # Passing through arguments to the Mel filters
-        S_dB = librosa.power_to_db(S, ref=max)
-        S_dB_out = librosa.util.fix_length(S_dB, 1067, axis=1)
+        self.S_dB = librosa.power_to_db(S, ref=max)
+        S_dB_out = librosa.util.fix_length(self.S_dB, 1067, axis=1)
         ms_delta_dB = librosa.power_to_db(ms_delta, ref=max)
         ms_delta_dB_out = librosa.util.fix_length(ms_delta_dB, 1067, axis=1)
         ms_delta2_dB = librosa.power_to_db(ms_delta2, ref=max)
         ms_delta2_dB_out = librosa.util.fix_length(ms_delta2_dB, 1067, axis=1)
-        output = dstack((S_dB_out, ms_delta_dB_out, ms_delta2_dB_out))
+        self.output = dstack((S_dB_out, ms_delta_dB_out, ms_delta2_dB_out))
+        self.displaySpectrogram()
 
-
-        #Plotting the Mel Spectrogram
-        # plt.figure(figsize=(10, 4))
-        # librosa.display.specshow(S_dB, x_axis='time',
-        #                          y_axis='mel', sr=samplerate,
-        #                          fmax=8000)
-        # plt.colorbar(format='%+2.0f dB')
-        # plt.title('Mel-frequency spectrogram')
-        # plt.tight_layout()
-        # plt.show()
-        self.saveFile(output)
-        return output
+    def displaySpectrogram(self):
+        # Plotting the Mel Spectrogram
+        plt.figure(figsize=(10, 4))
+        librosa.display.specshow(self.S_dB, x_axis='time',
+                                 y_axis='mel', sr=self.samplerate,
+                                 fmax=8000)
+        plt.colorbar(format='%+2.0f dB')
+        plt.title('Mel-frequency spectrogram')
+        plt.tight_layout()
+        plt.show()
+        self.saveFile(self.output)
+        return(self.output)
 
     def saveFile(self, contents):
         unique_num = int(time.time())
