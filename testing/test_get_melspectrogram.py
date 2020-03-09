e025dcead63559ac1517d54ba83daec4a6766893
Jo Slayton <ajs41@missouristate.edu>
2020-03-08 19:33:01 -0500

    initial commit of test_get_melspecrogram.py

---
+++ testing/test_get_melspectrogram.py
@@ -0,0 +1,20 @@
+'''
+CSC450 SP2020 Group 4
+03/08/2020
+'''
+'''
+test cases:
+    Shape of Array
+    Size of Array
+    Array is saved 
+    umm idk what else
+'''
+
+def bruh():
+    print("bruh moment.")
+
+def main():
+    bruh()
+
+if __name__ == '__main__':
+    main()
