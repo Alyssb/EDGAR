06b038aaf563355739798f18bdc9b8f649123345
Jo Slayton <ajs41@missouristate.edu>
2020-03-08 20:24:20 -0500

    tired of working on this today

--- testing/test_get_melspectrogram.py
+++ testing/test_get_melspectrogram.py
@@ -1,16 +1,21 @@
 '''
 CSC450 SP2020 Group 4
 03/08/2020
+
+MUST be run from EDGAR directory.
 '''
 '''
 test cases:
     That it works
+    Deletes audio file
+    Creates .npy file
     Shape of Array
     Size of Array
     Array is saved 
     umm idk what else
 '''
 import sys
+from os.path import exists
 
 # adds string to path for the running of this file
 sys.path.insert(1, "C:\\Users\\alyss\\Documents\\EDGAR\\")
