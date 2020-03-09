2fba2b9cff98453a0b98ee694eb1ff0f32c8e996
Jo Slayton <ajs41@missouristate.edu>
2020-03-08 19:43:52 -0500

    able to access get_melspectrogram.py from test_get_melspectrogram.pyt

--- testing/test_get_melspectrogram.py
+++ testing/test_get_melspectrogram.py
@@ -4,17 +4,24 @@ CSC450 SP2020 Group 4
 '''
 '''
 test cases:
+    That it works
     Shape of Array
     Size of Array
     Array is saved 
     umm idk what else
 '''
+import sys
 
-def bruh():
+# adds string to path for the running of this file
+sys.path.insert(1, "C:\\Users\\alyss\\Documents\\EDGAR\\")
+
+import get_melspectrogram
+
+def default_test():
     print("bruh moment.")
 
 def main():
-    bruh()
+    default_test()
 
 if __name__ == '__main__':
     main()
