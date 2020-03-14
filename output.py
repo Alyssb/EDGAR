0099f0d2e92c45e9a792f0f978a22f5ea6983355
Jo Slayton <ajs41@missouristate.edu>
2020-03-14 10:36:03 -0500

    may be hurting more than helping but the deed is done

--- output.py
+++
@@ -1,4 +0,0 @@
-def response(result):
-    true_label = ['anger', 'disgust', 'excited', 'fear', 'frustrated',
-                  'happy', 'neutral', 'other', 'sad', 'surprised', 'xxx']#
-    print("The model has determined that you just spoke in the emotion of: ", true_label[result])
