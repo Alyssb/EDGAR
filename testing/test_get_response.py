dae5e587aa0cec437ecb5eed8a6913887744c78b
Jo Slayton <ajs41@missouristate.edu>
2020-03-28 18:52:33 -0500

    implemented stubs for test_get_response.py

--- testing/test_get_response.py
+++ testing/test_get_response.py
@@ -1,10 +1,44 @@
 '''
-Alyssa Slayton
+CSC450 SP2020 Group 4
 03/28/2020
+
+MUST be run from EDGAR directory.
+'''
+'''
+test cases:
+    happy
+    fearful
+    angry
+    sad
+    neutral
+    invalid  input
 '''
 
+def test_happy():
+    print("not yet implemented")
+
+def test_fear():
+    print("not yet implemented")
+
+def test_angry():
+    print("not yet implemented")
+
+def test_sad():
+    print("not yet implemented")
+
+def test_neutral():
+    print("not yet implemented")
+
+def test_invalid():
+    print("not yet implemented")
+
 def main():
-    print("hello world.")
+    test_happy()
+    test_fear()
+    test_angry()
+    test_sad()
+    test_neutral()
+    test_invalid()
 
 if __name__ == "__main__":
     main()
