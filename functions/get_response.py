2eb621b8a8035c8e16f65090ab5ad9836daef810
Jo Slayton <ajs41@missouristate.edu>
2020-03-28 18:40:24 -0500

    wrapping get_response.py in a class

--- functions/get_response.py
+++ functions/get_response.py
@@ -8,58 +8,62 @@ import tkinter
 import cv2
 import PIL.Image, PIL.ImageTk
 
-# set the response that will call the correct image for the emotion displayed
-def set_emotion(emo):
-    if emo == 1:
-        response = "happy.jpeg"
-    if emo == 2:
-        response = "fear.jpeg"
-    if emo == 3:
-        response = "mad.jpeg"
-    if emo == 4:
-        response = "sad.jpeg"
-    if emo == 5:
-        response = "neutral.jpeg"
+class get_response.py:
+    def __init__(self, emo_num): # delete emo_num once get_classification is implemented
 
-    return response
+        # self.classification = get_classification() #<--- uncomment and delete next line once get_classification is implemented
+        self.classification = emo_num
+
+    # set the filename for the proper image file
+    def set_emotion(self):
+        if self.classification == 1:
+            self.response = "happy.jpeg"
+        elif self.classification == 2:
+            self.response = "fear.jpeg"
+        elif self.classification == 3:
+            self.response = "mad.jpeg"
+        elif self.classification == 4:
+            self.response = "sad.jpeg"
+        elif self.classification == 5:
+            self.response = "neutral.jpeg"
+        else:
+            print(self.emo, " is not a valid classification.")
 
 
-def get_response(emo_num):  # emo_num can be removed once the get_classification function has been written
+    def print_response(self):
 
-    window = tkinter.Tk()
-    window.title("Emotion Detected")
+        window = tkinter.Tk()
+        window.title("Emotion Detected")
 
-    # classification = get_classification() <--- This would be how the responder gets the classification
-    classification = emo_num  # set to number 1 till get_classification() created
+        responseFilePath = "C:\\Users\\alyss\\Documents\\EDGAR\\response_image\\" #CHANGE FILE PATH
 
-    responseFilePath = "C:\\Users\\alyss\\Documents\\EDGAR\\response_image\\" #CHANGE FILE PATH
+        set_emotion(classification)
 
-    emotion = set_emotion(classification)
+        # Load an image using OpenCV
+        cv_img = cv2.cvtColor(cv2.imread(responseFilePath + emotion), cv2.COLOR_BGR2RGB)
 
-    # Load an image using OpenCV
-    cv_img = cv2.cvtColor(cv2.imread(responseFilePath + emotion), cv2.COLOR_BGR2RGB)
+        # Get the image dimensions (OpenCV stores image data as NumPy ndarray)
+        height, width, no_channels = cv_img.shape
 
-    # Get the image dimensions (OpenCV stores image data as NumPy ndarray)
-    height, width, no_channels = cv_img.shape
+        # Create a canvas that can fit the above image
+        canvas = tkinter.Canvas(window, width = width, height = height)
+        canvas.pack()
 
-    # Create a canvas that can fit the above image
-    canvas = tkinter.Canvas(window, width = width, height = height)
-    canvas.pack()
+        # Use PIL (Pillow) to convert the NumPy ndarray to a PhotoImage
+        photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(cv_img))
 
-    # Use PIL (Pillow) to convert the NumPy ndarray to a PhotoImage
-    photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(cv_img))
+        # Add a PhotoImage to the Canvas
+        canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
 
-    # Add a PhotoImage to the Canvas
-    canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
-
-    # Run the window loop
-    window.mainloop()
+        # Run the window loop
+        window.mainloop()
 
 # This will call all 5 response. Close image to get next image to display - only needed for testing purposes
-
-
 get_response(1)
 get_response(2)
 get_response(3)
 get_response(4)
 get_response(5)
+
+def main():
+    print("Main function of get_response.py")
