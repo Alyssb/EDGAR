b0ea7ccd6bcf551ef963c1d06c93cac1b3090420
Jo Slayton <ajs41@missouristate.edu>
2020-03-30 21:54:27 -0500

    done for the night...

--- functions/decibel_detection.py
+++ functions/decibel_detection.py
@@ -4,36 +4,9 @@ Detects the decibel level and activates EDGAR if 60dB is exceeded
 '''
 
 import pyaudio
-import numpy as np
-import pylab
-import time
 
-RATE = 44100
-CHUNK = int(RATE/20) # RATE / number of updates per second
+def main():
+    print("hello world")
 
-def soundplot(stream):
-    t1=time.time()
-    data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
-    pylab.plot(data)
-    pylab.title(i)
-    pylab.grid()
-    pylab.axis([0,len(data),-2**16/2,2**16/2])
-    pylab.savefig("03.png",dpi=50)
-    pylab.close('all')
-    print("took %.02f ms"%((time.time()-t1)*1000))
-
-if __name__=="__main__":
-    p=pyaudio.PyAudio()
-    stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
-                  frames_per_buffer=CHUNK)
-    for i in range(int(20*RATE/CHUNK)): #do this for 10 seconds
-        soundplot(stream)
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-# def main():
-#     print("hello world")
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
