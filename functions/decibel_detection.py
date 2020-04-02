ef26cbd47f2b7071ab742562df0de33c137ee4ff
Jo Slayton <ajs41@missouristate.edu>
2020-04-01 20:09:01 -0500

    no longer sped up

--- functions/decibel_detection.py
+++ functions/decibel_detection.py
@@ -16,15 +16,13 @@ chunk = 1024
 sample_format = pyaudio.paInt16
 channels = 1
 fs = 44100
+
+unique_num = int(time.time())
+tempnum = unique_num + 9
+final_time = unique_num + 3
 frames = []
 
-while temp:
-    unique_num = int(time.time())
-    final_time = unique_num + 3
-    tempnum = unique_num + 9
-    print(unique_num)
-    print(final_time)
-
+while time.time() < tempnum:
     filename = 'live_audio/Output' + str(unique_num) + '.wav'
 
     p = pyaudio.PyAudio()
@@ -35,24 +33,30 @@ while temp:
                     frames_per_buffer=chunk,
                     input=True)
 
+    while time.time() < final_time:
+        data = stream.read(chunk)
+        frames.append(data)
+
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    b = ''
+    wf.writeframes(b''.join(frames))
+    #print(b''.join(frames))
+    wf.close()
+    p.terminate()
+    print(filename + " saved")
+
+    update_loop()
+
+    temp = False
 
 
-    data = stream.read(chunk)
-    frames.append(data)
-
-    print(time.time())
-    if final_time < time.time():
-        wf = wave.open(filename, 'wb')
-        wf.setnchannels(channels)
-        wf.setsampwidth(p.get_sample_size(sample_format))
-        wf.setframerate(fs)
-        b = 1
-        wf.writeframes(b''.join(frames))
-        #print(b''.join(frames))
-        wf.close()
-        p.terminate()
-        print(filename + " saved")
-        temp = False
+def update_loop():
+    frames = []
+    unique_num = int(time.time())
+    final_time = int(time.time()) + 3
 
 # def main():
 #     print("hello world")
