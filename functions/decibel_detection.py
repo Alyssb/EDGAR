5e72c2933e58757f6b4d334bc8fa308ff72bee89
Jo Slayton <ajs41@missouristate.edu>
2020-04-11 01:58:34 -0500

    tried to document decibel_detection.py professionally

--- functions/decibel_detection.py
+++ functions/decibel_detection.py
@@ -1,6 +1,10 @@
 '''
-CSC450 Team 4
-Detects the decibel level and activates EDGAR if 60dB is exceeded
+CSC450 SP2020 Group 4
+Missouri State University
+
+Detects and creates a recording if the decibel level of 60dB is exceeded
+Exits if 'Q' is pressed while listening
+MUST be run from EDGAR directory
 '''
 # ***************************** imports *****************************
 # general imports
@@ -32,12 +36,19 @@ class do_record():
     def __init__(self):
         print("EDGAR has started.")
 
-    # sets up a fresh recording
-    def setup_record(self):
-        '''
-        self.frames[]:      array for storing frames during recording
+    '''
+    function: setup_record
 
-        '''
+    sets up a fresh recording stream
+    prints system is ready
+    must be executed before check_dB
+
+    class variables:
+        frames (array):             array for storing frames during recording
+        p (PyAudio):                instance of PyAudio
+        stream (PyAudio stream):    PyAudio stream created with global variables
+    '''
+    def setup_record(self):
         self.frames = []
         self.p = pyaudio.PyAudio()
 
@@ -49,7 +60,21 @@ class do_record():
                     input=True,
                     output=True)  
         print('EDGAR is ready')  
-                    
+
+    '''
+    function: check_dB
+
+    if current decibel level above THRESHOLD, make a recording
+    if key 'Q' is pressed, exit
+
+    class variables:
+        unique_num (int):   current time in seconds
+        filename (string):  name of file to store recording in
+    
+    local variables:
+        input (frame):      the current audio chunk
+        rms_val (float):    the rms value of input
+    '''       
     def check_dB(self):
         print('listening... press \'Q\' to quit')
         while True:
@@ -62,14 +87,34 @@ class do_record():
 
                 if rms_val > THRESHOLD:
                     print("sound detected. initiating record at time", time.time(), "\n")
+                    
                     self.unique_num = int(time.time())
                     self.filename = 'live_audio/Output' + str(self.unique_num) + '.wav'
-                    self.final_time = self.unique_num + 3
                     self.record_3sec()
+                    
                     print("preparing to resume listening. press \'Q\' to quit")
-                    self.setup_record()
+                    self.setup_record()     # sets up a new recording and clears self.frames
+    
+    '''
+    function: rms
 
-    # calculates RMS of current frame
+    calculates RMS (root-mean-square) of current frame
+
+    parameters:
+        frame (frame): the current chunk
+    
+    returns:
+        rms (float):    calculated RMS value for frame
+
+    local variables:
+        count (float):          length of frame divided by short width (SWIDTH)
+        format (string):        format string for frame (short int)
+        shorts (tuple):         tuple that contains short ints representing frame
+        sample (tuple element): an iteration through shorts
+        n (float):              sample times short int normalization factor (SHORT_NORMALIZE)
+        sum_squares (float):    sum of n * n for for shorts
+        rms (float):            represents calculated RMS for frame   
+    '''
     def rms(self, frame):
         count = len(frame) / SWIDTH
         format = "%dh" % (count)
@@ -83,12 +128,29 @@ class do_record():
 
         return rms * 1000
 
+    '''
+    function: record_3sec
+
+    appends all frames to frames for 3 seconds
+    calls write_to_file()
+
+    local variables:
+        data (frame):   the value of the current chunk
+    '''
     def record_3sec(self):
-        while(time.time() < self.final_time):
+        while(time.time() < (self.unique_num + 3)):
             data = self.stream.read(CHUNK)
             self.frames.append(data)
-        # self.write_to_file()
+        self.write_to_file()
 
+    '''
+    function: write_to_file
+
+    saves recorded audio as a WAV file
+
+    local variables:
+        wf (wave object):   filename opened as an empty wave object
+    '''
     def write_to_file(self):
         wf = wave.open(self.filename, 'wb')
         wf.setnchannels(CHANNELS)
@@ -104,6 +166,7 @@ class do_record():
 
 # ***************************** main *****************************
 def main():
+    # executes the do_record class
     record_instance = do_record()
     record_instance.setup_record()
     record_instance.check_dB()
