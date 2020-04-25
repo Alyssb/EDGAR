6771e7476d7575909fbaf738df002967cb047cc5
MJelinek <jelinek076@missouristate.edu>
2020-04-25 17:36:20 -0500

    updated get_melspectrogram and get_mfcc classes

--- functions/get_melspectrogram.py
+++ functions/get_melspectrogram.py
@@ -11,10 +11,10 @@ currently called by EDGAR_demo.py
 # You will need to pip install all of these things
 import librosa
 import librosa.display
-from numpy import max, dstack, save, frombuffer, set_printoptions, array, uint8, float64
+from numpy import max, save, frombuffer, uint8
 import matplotlib
 matplotlib.use('Agg')
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas, FigureCanvasAgg
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import time
 import matplotlib.pyplot as plt
 
@@ -24,7 +24,6 @@ from os import remove
 from os.path import exists
 
 # ***************************** class melSpectrogram *****************************
-from scipy import io
 
 
 class melSpectrogram:
@@ -48,31 +47,6 @@ class melSpectrogram:
         # Convert a power spectrogram (amplitude squared) to decibel (dB) units
         self.S_dB = librosa.power_to_db(self.S, ref=max)
 
-        print("Mel Spectrogram")
-        print(self.S_dB)
-
-        #self.padToLongest()
-
-        # CAN ONLY PLOT ONE FIGURE IN A PYTHON SCRIPT. 
-        # uncomment only if there will be only one audio file and you want it displayed
-        #self.saveSpectrogram(melSpectrogram_nparray)
-
-    def padToLongest(self):
-        # pads numpy arrays with zeroes to fit longest wav file used for training (1067) rows
-        self.S_dB_out = librosa.util.fix_length(self.S_dB, 1067, axis=1)
-        self.ms_delta_dB_out = librosa.util.fix_length(self.ms_delta_dB, 1067, axis=1)
-        self.ms_delta2_dB_out = librosa.util.fix_length(self.ms_delta2_dB, 1067, axis=1)
-
-    def displaySpectrogram(self):
-        # Plotting the Mel Spectrogram
-        plt.figure(figsize=(10, 4))
-        librosa.display.specshow(self.S_dB, x_axis='time',
-                                 y_axis='mel', sr=self.samplerate,
-                                 fmax=8000)
-        plt.colorbar(format='%+2.0f dB')
-        plt.title('Mel-frequency spectrogram')
-        plt.tight_layout()
-        plt.show()
 
     def saveSpectrogram(self):
         # Save spectrogram as rgb numpy array
@@ -80,17 +54,12 @@ class melSpectrogram:
         canvas = FigureCanvas(fig)
         ax = fig.add_subplot(111)
         librosa.display.specshow(librosa.amplitude_to_db(self.S_dB, ref=max), ax=ax, y_axis='log', x_axis='time')
-        fig.savefig('spec.png')
         canvas.draw()
-        data = frombuffer(fig.canvas.tostring_rgb(), dtype=uint8)
-        print("data1")
-        print(data)
-        data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-        print("data2")
-        print(data)
+        self.data = frombuffer(fig.canvas.tostring_rgb(), dtype=uint8)
+        self.data = self.data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
 
-    def saveFile(self, image):
-        # use current time to calculate a unique numbe
+    def saveFile(self):
+        # use current time to calculate a unique number
         unique_num = int(time.time())
         self.filename = '/Users/Momma/PycharmProjects/EDGAR/numpy_output/Output' + str(unique_num)
         save(self.filename, self)
@@ -108,12 +77,14 @@ class melSpectrogram:
 # ***************************** main *****************************
 def main():
     print("main function of get_melspectrogram.py")
+
+    # create instance of melSpectrogram
     mSpec = melSpectrogram("Ses01F_impro03_F005.wav")
+
+    # create melSpectrogram metric from file
     mSpec.get_MelSpectrogram()  # creates a mel spectrogram for a given file
 
-    print("ms from main")
-    print(mSpec.S_dB)
-    #mSpec.saveFile()  # Saves 3D numpy output array to a file
+    # save melspectrogram as a 3D numpy array for processing
     mSpec.saveSpectrogram()
 
 
--- functions/get_mfcc.py
+++ functions/get_mfcc.py
@@ -2,7 +2,7 @@
 '''
 CSC450 SP 2020 Group 4
 03/08/2020
-creates, stores, and returns a spectrogram for a given audio file
+creates, stores, and returns mfcc metrics for a given audio file
 
 currently called by EDGAR_demo.py
 '''
@@ -11,9 +11,10 @@ currently called by EDGAR_demo.py
 # You will need to pip install all of these things
 import librosa
 import librosa.display
-from numpy import max, dstack, save, frombuffer
+from numpy import save, frombuffer, uint8
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import time
 
@@ -42,47 +43,20 @@ class mfcc:
         # an ortho - normal DCT basis.
         # lifter: number >= 0 (cepstral filtering)
 
-        mfccs = librosa.feature.mfcc(y=y, sr=self.samplerate, n_mfcc=40)
-
-        return(self.saveSpectrogram())
-
-    def padToLongest(self):
-        # pads numpy arrays with zeroes to fit longest wav file used for training (1067) rows
-        self.S_dB_out = librosa.util.fix_length(self.S_dB, 1067, axis=1)
-        self.ms_delta_dB_out = librosa.util.fix_length(self.ms_delta_dB, 1067, axis=1)
-        self.ms_delta2_dB_out = librosa.util.fix_length(self.ms_delta2_dB, 1067, axis=1)
-
-    def displaySpectrogram(self):
-        # Plotting the MFCC
-        plt.figure(figsize=(10, 4))
-        librosa.display.specshow(self.S_dB, x_axis='time',
-                                 y_axis='mfcc', sr=self.samplerate,
-                                 fmax=8000)
-        plt.colorbar(format='%+2.0f dB')
-        plt.title('MFCC')
-        plt.tight_layout()
-        plt.show()
+        self.mfccs = librosa.feature.mfcc(y=y, sr=self.samplerate, n_mfcc=40)
 
     def saveSpectrogram(self):
         # Save spectrogram as rgb numpy array
-        fig = Figure()
-        fig.tight_layout(pad=0)
+        fig = plt.Figure()
         canvas = FigureCanvas(fig)
-
-        ax = fig.gca()
-        ax.axis('off')
-        ax.margins(0)
-
+        ax=fig.add_subplot(111)
         canvas.draw()  # draw the canvas, cache the renderer
 
         #makes a 1d array of rgb values
-        image = frombuffer(canvas.tostring_rgb(), dtype='uint8')
+        self.data = frombuffer(fig.canvas.tostring_rgb(), dtype=uint8)
 
         #converts to a 3d array of rgb values
-        formatted_image = image.reshape(fig.canvas.get_width_height()[::-1]+(3,))
-        print("shape of formatted image: ", formatted_image.shape)
-
-        return formatted_image
+        self.data = self.data.reshape(fig.canvas.get_width_height()[::-1]+(3,))
 
     def saveFile(self, image):
         # use current time to calculate a unique number
@@ -103,12 +77,17 @@ class mfcc:
 # ***************************** main *****************************
 def main():
     print("main function of get_mfcc.py")
-    mfcc_metrics = mfcc("Ses01F_impro03_F005.wav")
-    mfcc_nparray = mfcc_metrics.get_mfcc()  # creates a mfcc for a given file
 
-    #mSpec.saveFile()  # Saves 3D numpy output array to a file
-    mfcc_metrics.saveFile(mfcc_metrics.get_mfcc())
+    # create instance of mfcc
+    mfcc_instance = mfcc("Ses01F_impro03_F005.wav")
 
+    # create mfcc metric from file
+    mfcc_instance.get_mfcc()  # creates a mfcc for a given file
+
+    # save mfcc as a 3D numpy array for processing
+    mfcc_instance.saveSpectrogram()
+
+    print(mfcc_instance.data)
 
 if __name__ == '__main__':
     main()
