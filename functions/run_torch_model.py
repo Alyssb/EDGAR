f36a4120fe27aa67d988cb7de879a2accea89f1a
Jo Slayton <ajs41@missouristate.edu>
2020-04-26 23:20:18 -0500

    documented run_torch_model.py

--- functions/run_torch_model.py
+++ functions/run_torch_model.py
@@ -1,95 +1,58 @@
 '''
-Author: Cory &
-
-Load saved model, run stuff through saved model
+CSC450 SP2020 Group 4 (Cory Jackson)
+Missouri State University
+Loads a saved model
+Runs a spectrogram through loaded model
 '''
-
-#from __future__ import print_function, division
-
-import torch
-import torch.nn as nn
-import torchvision
-from torchvision import transforms
+# ********************************** imports **********************************
+# general imports
 import numpy as np
 import os
 import time
 
-device = torch.device("cpu" if not (torch.cuda.is_available()) else "cuda:0")
-#torch.nn.Module.dump_patches = True
+# pytorch imports3
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import transforms
 
+# ********************************** end imports **********************************
+
+# sets the device to either the available GPU or system CPU
+device = torch.device("cpu" if not (torch.cuda.is_available()) else "cuda:0")
+
+# ********************************** class loadModel **********************************
 def loadModel(metrics):
     
-    model = torch.load("testmodelsavewhole4.pt", map_location=device) #whole model, 4/10- use this not state, state is unstable it seems
-#start of state code
-##    model = torchvision.models.resnet18(pretrained=True)
-##    # "Freeze" the weights on the pretrained model.
-##    for param in model.parameters():
-##        param.requires_grad = False
-##
-##    # Parameters of newly constructed modules have requires_grad=True by default
-##    num_ftrs = model.fc.in_features
-##    # Adding layers on the end of the pretrained model.
-##    # These layers will be the only layers trained while the model is running
-##    model.fc = nn.Linear(num_ftrs, 10)
-##
-##    # Sending the model to the device it will be trained on
-##    model = model.to(device)
-#end of state code
+    '''
+    I don't have the energy to modularize this class right now
+    it's just going to be like this.
+    '''
+    # load and evaluate model (make function)
+    model = torch.load("testmodelsavewhole4.pt", map_location=device)
+    model.eval()
 
-    
-    model.eval() #internet docs say you gotta do this, so do this. No I don't know why stop asking so many questions
-
+    # transform passed numpy array into a tensor
     metrics = np.copy(metrics)
     metrics = torch.from_numpy(metrics)
     
+    # reform tensor into correct shape
     metrics = metrics.unsqueeze(0)
-    # metrics = metrics.unsqueeze(0)
-
-    # print(metrics.shape) # [1,1,40,98]
     metrics = metrics.permute(0,3,1,2) #0,3,1,2 or 0,3,2,1 0=1,1=40, 2=224, 3=3
     
+    # send to device for analysis
     metrics = metrics.to(device)
     model = model.to(device)
     metrics = metrics.float()
 
+    # run model on tensor
     output = model(metrics)
 
-    prediction = int(torch.argmax(output))
-    #print("this be the prediction ",prediction)
-    
+    # get predicted emotion
+    prediction = int(torch.argmax(output))    
     
     return prediction
 
 
 if __name__ == '__main__':
-    print("welcome to run_torch_model.py")
-##    since = time.time()
-##    #test code below
-##    tester = np.load("testanger.npy")
-##    print("ang(0?): ")
-##    loadModel(tester)
-##    time_elapsed = time.time() - since
-##    print('Training complete in {:.0f}m {:.0f}s'.format(
-##        time_elapsed // 60, time_elapsed % 60))
-##    
-##    tester = np.load("testfear.npy")
-##    print("fear(1?): ")
-##    loadModel(tester)
-##    
-##    tester = np.load("testhap.npy")
-##    print("hap(2?): ")  
-##    loadModel(tester)
-##    
-##    tester = np.load("testneu.npy")
-##    print("neu: (3?)")
-##    loadModel(tester)
-##    
-##
-##
-##    tester = np.load("testsad.npy")
-##    print("sad(4?): ")
-##    loadModel(tester)
-##
-##    time_elapsed = time.time() - since
-##    print('Training complete in {:.0f}m {:.0f}s'.format(
-##        time_elapsed // 60, time_elapsed % 60))
+    print("main function of run_torch_model.py")
