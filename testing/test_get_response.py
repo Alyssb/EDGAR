471693d6f6dba9848379bcdaee01e2ab6fb88057
Jo Slayton <ajs41@missouristate.edu>
2020-05-03 01:57:33 -0500

    fixed existing test cases in test_get_response.py

--- testing/test_get_response.py
+++ testing/test_get_response.py
@@ -20,47 +20,79 @@ from test suite:
 
 '''
 import sys
-sys.path.insert(1, "C:\\Users\\alyss\\Documents\\EDGAR\\functions\\")
+sys.path.append('./functions/')
 
 import get_response
 
-def test_happy():
-    classification = 1
-    responseObject = get_response.get_response(classification)
-    responseObject.get_output()
-    
-
-def test_fear():
-    classification = 2
-    responseObject = get_response.get_response(classification)
-    responseObject.get_image()
-
-def test_angry():
-    classification = 3
-    responseObject = get_response.get_response(classification)
-    responseObject.get_output()
-
-def test_sad():
-    classification = 4
-    responseObject = get_response.get_response(classification)
-    responseObject.get_output()
-
-def test_neutral():
-    classification = 5
-    responseObject = get_response.get_response(classification)
-    responseObject.get_output()
-
-def test_invalid():
+def test_anger():
+    print("\nRunning test_anger")
     classification = 0
     responseObject = get_response.get_response(classification)
     responseObject.get_output()
+    if responseObject.response == "ANGER":
+        print("\ttest_anger passed")
+    else:
+        print("\ttest_anger failed")
+    
+
+def test_fear():
+    print("\nRunning test_fear")
+    classification = 1
+    responseObject = get_response.get_response(classification)
+    responseObject.get_output()
+    if responseObject.response == "FEAR":
+        print("\ttest_fear passed")
+    else:
+        print("\ttest_fear failed")
+
+
+def test_happy():
+    print("\nRunning test_happy")
+    classification = 2
+    responseObject = get_response.get_response(classification)
+    responseObject.get_output()
+    if responseObject.response == "HAPPY":
+        print("\ttest_happy passed")
+    else:
+        print("\ttest_happy failed")
+
+
+def test_neutral():
+    print("\nRunning test_neutral")
+    classification = 3
+    responseObject = get_response.get_response(classification)
+    responseObject.get_output()
+    if responseObject.response == "NEUTRAL":
+        print("\ttest_neutral passed")
+    else:
+        print("\ttest_neutral failed")
+
+
+
+def test_sad():
+    print("\nRunning test_sad")
+    classification = 4
+    responseObject = get_response.get_response(classification)
+    responseObject.get_output()
+    if responseObject.response == "SAD":
+        print("\ttest_sad passed")
+    else:
+        print("\ttest_sad failed")
+
+
+def test_invalid():
+    classification = 5
+    responseObject = get_response.get_response(classification)
+    responseObject.get_output()
+    print("\ttest_invalid passed if output was:\t5 is not a valid classification")
+
 
 def main():
-    test_happy()
+    test_anger()
     test_fear()
-    test_angry()
-    test_sad()
+    test_happy()
     test_neutral()
+    test_sad()
     test_invalid()
 
 if __name__ == "__main__":
